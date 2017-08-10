06577d3030621174325a09e83532bec6b573457b
Greg doud <gpdoud@gmail.com>
2017-08-10 13:49:18 -0400

    added edit check on email using jquery

--- js/jquery-friends.js
+++ js/jquery-friends.js
@@ -2,6 +2,24 @@ $().ready(function() {
 
 	$("*").css("font-size","18pt").css("text-align","center");
 	$("table").css("width","100%").css("margin","20px 0 0 0");
+	$("#msg").css("color","red").css("font-weight","bold");
+
+	$("#Email").blur(function() {
+		var email = $(this).val();
+		if(email.indexOf('@') == -1) {
+			$(this).focus();
+			$("#msg").text("An email must contain an at sign (@)");
+		} else {
+			var pos = email.indexOf("@");
+			if(email.indexOf(".",pos) == -1) {
+				$(this).focus();
+				$("#msg").text("An email must contain a period after the at sign (@)");
+			} else {
+				$("#msg").text(" ");
+			}
+		}
+	});
+
 	$("button").click(function() {
 		var friends = [];
 		// var friendObj = {
