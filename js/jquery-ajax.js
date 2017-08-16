c3b855e6afa5bec581e6b3b61e4440bd0f54d75e
Greg doud <gpdoud@gmail.com>
2017-08-16 08:36:38 -0400

    updated jquery-ajax

--- js/jquery-ajax.js
+++ js/jquery-ajax.js
@@ -1,12 +1,18 @@
 $().ready(function() {
-	var url = 'http://prs.gregorydoud.net/Users/Get/1';
+	var url = 'http://prs.gregorydoud.net/Users/List';
 
 	$("body").css('font-size','26pt');
 
 	$('button').click(function() {
+
+		console.log("Before the data is asked for.");
+		
 		$.get(url).done(function(resp) {
-			console.log(resp);
+			console.log("The data returned.");
 		});
+
+		console.log("After the data is asked for.");
 	});
+
 
 });
